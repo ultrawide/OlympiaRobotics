3b739291674d7f2d43e32dda5f3fc2983a8cb497
Magellan <9584410+edwin3663@users.noreply.github.com>
2018-09-26 22:47:05 -0700

    Update client_cv2.py

--- petergui/client_cv2.py
+++ petergui/client_cv2.py
@@ -11,22 +11,13 @@ i = 0
 
 cam = cv2.VideoCapture(0)
 
-# encode 2 different images
-#f0 = open("mountains1.jpg", 'rb')
-#f1 = open("mountains2.jpg", 'rb')
-#bytes1 = bytearray(f0.read())
-#image1 = base64.b64encode(bytes1)
-#bytes2 = bytearray(f1.read())
-#image2 = base64.b64encode(bytes2)
-#f0.close()
-#f1.close()
-
 while True:
 
 	try:
 		ret_val, img = cam.read()
-		cv2.imshow('my webcam', img)
-		#img_scaled = cv2.resize(img, (640,480))
+		#cv2.imshow('my webcam', img)
+		#cv2.waitKey()
+		img_scaled = cv2.resize(img, (640,480))
 		cv2.imwrite('image_c.jpg', img_scaled)
 		
 		f = open('image_c.jpg', 'rb')
@@ -34,7 +25,7 @@ while True:
 		image = base64.b64encode(bytes)
 		socket.send(image)
 
-		time.sleep(.1)
+		time.sleep(.05)
 		socket.recv()
 
 	except KeyboardInterrupt:
