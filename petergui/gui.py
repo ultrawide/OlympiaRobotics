39d1641666be271c07bc0bdd0a91b36db493a1e0
Frank Su <fpsu@sfu.ca>
2018-10-09 14:58:16 -0700

    Added Thread1 now we can have 2 camera streams on the UI

--- petergui/gui.py
+++ petergui/gui.py
@@ -10,19 +10,20 @@ import base64
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)  # using rep and req model. We can improve this
-socket.bind("tcp://207.23.166.50:8000")
-
+socket2 = context.socket(zmq.REP)
 
 # This thread reads the image from the robot's camera
 class Thread(QThread):
 	sig1 = pyqtSignal()
-
+	socket.bind("tcp://207.23.201.138:8000")
 	def __init__(self, parent=None):
 		super(QThread, self).__init__()
+		
 
 	def run(self):
 		self.running = True
-
+		
+		
 		while self.running:
 			image = socket.recv()
 			socket.send_string('')  # sends back an empty string
@@ -31,10 +32,28 @@ class Thread(QThread):
 			#print(ba)
 			f.write(ba)
 			f.close()
-			print("received")
+			print("received image 1")
 			self.sig1.emit()
 			#time.sleep(1)
 
+class Thread1(QThread):
+	sig2 = pyqtSignal()
+	socket2.bind("tcp://207.23.201.138:8001")
+	def __init__(self, parent=None):
+		super(QThread, self).__init__()
+
+	def run(self):
+		self.running = True
+
+		while self.running:
+			image = socket2.recv()
+			socket2.send_string('')
+			f = open("image2.jpg", "wb")
+			ba = bytearray(base64.b64decode(image))
+			f.write(ba)
+			f.close()
+			print("received image 2")
+			self.sig2.emit()
 			
 class MainWindow(QWidget):
 
@@ -66,14 +85,21 @@ class MainWindow(QWidget):
 		#self.layout.addWidget(self.label, 0, 0)
 		self.setLayout(self.layout)
 		self.th = Thread(self)
+		self.th1 = Thread1(self)
 		self.th.start()
+		self.th1.start()
 		self.th.sig1.connect(self.on_change)
+		self.th1.sig2.connect(self.on_change1)
 		self.show()
 
 	def on_change(self):
 		self.my_image = QPixmap('image.jpg')
 		self.label.setPixmap(self.my_image)
-		self.label2.setPixmap(self.my_image)
+		#self.label2.setPixmap(self.my_image)
+
+	def on_change1(self):
+		self.my_image2 = QPixmap('image2.jpg')
+		self.label2.setPixmap(self.my_image2)
 
 	def handleButton(self):
 		print ('Hello World')
