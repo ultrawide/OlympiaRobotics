4940c41a314ef2fa5bfc981e22369a7f61b5351d
Magellan <9584410+edwin3663@users.noreply.github.com>
2018-11-05 16:14:21 -0800

    Update client_picamera.py

--- gui_controller/client_picamera.py
+++ gui_controller/client_picamera.py
@@ -11,29 +11,20 @@ import sys
 import Adafruit_PCA9685
 import smbus #for i2c
 import pigpio
-# Connect a client socket to my_server:8000 (change my_server to the
-# hostname of your server)
-my_server = '207.23.201.64' # edit me
-client_socket = socket.socket()
-client_socket.connect((my_server, 8001))  # For Robot 1 its 8000 and for robot 2 its 8001
-# Make a file-like object out of the connection
-connection = client_socket.makefile('wb')
-print("Established pipe for video stream")
 
-context = zmq.Context()
-socket = context.socket(zmq.REQ)
-socket.connect("tcp://" + my_server + ":8002")
-print("Established pipe for command listening")
+# Constants
+ARDUINO_I2C_ADDRESS = 0x04
 
-# For sending car count for robot 1
-socket2 = context.socket(zmq.REQ)
-socket2.connect("tcp://" + my_server + ":8004") # robot1: port 8003, robot2: port 8004 
+# TODO: be able to scan for the controllers IP address
+# or configure the networking on the controller so that
+# its ip is static.  What we want to do is turn on the
+# robot and have it connect to the controller without
+# having to manually edit settings.  I'm thinking we setup
+# another pi to act as a router.
+SERVER ADDRESS = '207.23.201.64' # Controller's IP Address
 
 # Communicate over I2C
 bus = smbus.SMBus(1) # 1 indicates /dev/i2c-1
-
-address = 0x08
-address_arduino = 0x04
 
 def writeNumber(address, value):
 	bus.write_byte(address, value)
@@ -59,6 +50,7 @@ def set_servo_pulse(channel, pulse):
     pwm.set_pwm(channel, 0, pulse)
     pwm.set_pwm_freq(50)
 #------------------------------- Lowers Robot stop hand  -----------------------
+
 def arm_down(cur_pos, end_pos):
     pos = cur_pos
     step_size = 10
@@ -110,21 +102,25 @@ def processCommand():
                 pwm.set_pwm(0, 0, 400)
                 pwm.set_pwm(1, 0, servo_min)
                 #socket.send(b"Set RoboFlagger to 'Stop' configuration")
+		
             elif message == "6":
                 print("set RoboFlagger to 'Slow' Configuration")
                 pwm.set_pwm(1, 0, servo_max)
                 arm_down(400, servo_min)
                 #pwm.set_pwm(1, 0, servo_max)
                 #socket.send(b"set RoboFlagger to 'Slow' configuration")
+		
             elif message == "8": # reset car count
                 print("Resetting car count from arduino")
-                writeNumber(address_arduino, 2)
+                writeNumber(ARDUINO_I2C_ADDRESS, 2)
                 time.sleep(0.05)
-                carCount = readNumber(address_arduino)
+                carCount = readNumber(ARDUINO_I2C_ADDRESS)
                 socket.send_string("")
                 message = socket.recv()
-            #else:
-                 #socket.send(b"Option not implemented")
+		
+            else:
+                 print("command not supported")
+			
     except KeyboardInterrupt:
         print("process command interrupted")
         try:
@@ -140,12 +136,13 @@ def carCount():
             message = socket2.recv()
             if message == "7": #get car count
 		print("Retrieving car count from arduino")
-		writeNumber(address_arduino, 1)
+		writeNumber(ARDUINO_I2C_ADDRESS, 1)
 		time.sleep(0.05)
-		carCount = readNumber(address_arduino)
+		carCount = readNumber(ARDUINO_I2C_ADDRESS)
 		print("From Arduino, I received car count: ", carCount)
 		socket2.send(str(carCount).encode('utf-8'))
 		message = socket2.recv()
+		
     except KeyboardInterrupt:
 	print("process command interrupted")
 	try:
@@ -155,12 +152,40 @@ def carCount():
 	    os._exit(0)
 
 if __name__ == "__main__":
-    video_thread = Thread(target = sendVideo)
-    video_thread.start()
-    print("Started video streaming thread")
-    
-    command_thread = Thread(target = processCommand)
-    command_thread.start()
+	# Socket for sending video frames to controller
+	client_socket = socket.socket()
+	client_socket.connect((my_server, 8001))  # For Robot 1 its 8000 and for robot 2 its 8001
+	# Make a file-like object out of the connection
+	connection = client_socket.makefile('wb')
+	print("Established pipe for video stream")
 
-    car_count_thread = Thread(target = carCount)
-    car_count_thread.start()
+	# User a configuration file to load the Robot's port configuration settings
+	# this way we can use the same python script for each robot.  Each robot
+	# will have a configuration file with its own custom settings.  See configuration.py
+	# for an example
+	
+	# Socket for recieving commands
+	context = zmq.Context()
+	socket = context.socket(zmq.REQ)
+	socket.connect("tcp://" + my_server + ":8002")
+	print("Established pipe for command listening")
+
+	# TODO Remove this socket 
+	# For sending car count for robot 1
+	socket2 = context.socket(zmq.REQ)
+	socket2.connect("tcp://" + my_server + ":8004") # robot1: port 8003, robot2: port 8004 	
+	
+	# pass the required socket to the thread in an argument
+	video_thread = Thread(target = sendVideo)
+	video_thread.start()
+	print("Started video streaming thread")
+
+	# pass the required socket to the thread in an argument
+	command_thread = Thread(target = processCommand)
+	command_thread.start()
+
+	# we need to combine the funcitonality of car count thread with
+	# the that of the command thread.  Command thread will be driven
+	# by the controller
+	car_count_thread = Thread(target = carCount)
+	car_count_thread.start()
