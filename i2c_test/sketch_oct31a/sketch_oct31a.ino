699040ac268c19c358c153116dc203563a2bf02a
ultrawide <colinchan02@gmail.com>
2018-11-07 15:37:09 -0800

    Integrated IR Sensor Detection

--- i2c_test/sketch_oct31a/sketch_oct31a.ino
+++ i2c_test/sketch_oct31a/sketch_oct31a.ino
@@ -1,11 +1,28 @@
+#include <IRremote.h> // Refer to readme.txt to install this library
 #include <Wire.h>
 
 #define SLAVE_ADDRESS 0x04
+
+// Car Counting US Sensor PINS
+int ECHOPIN = 7;
+int TRIGPIN = 6;
+
+// CarCount Variables
 int number = 0;
-int state = 0;
+int distance = 0;
+int duration = 0;
+int carCount = 0;
+bool enableCount = false;
+
+// IR SENSOR PINS
+int RECVPIN = 12;
+int LEDPIN = 3;
+
+// IR Sensor Variables
+IRrecv irrecv(RECVPIN);
+decode_results results;
 
 void setup() {
-  //pinMode(13, OUTPUT);
   Serial.begin(9600); // start serial for output
   // initialize i2c as slave
   Wire.begin(SLAVE_ADDRESS);
@@ -14,11 +31,22 @@ void setup() {
   Wire.onReceive(receiveData);
   Wire.onRequest(sendData);
   
-  Serial.println(“Ready!”);
+  Serial.println("I2C Ready!");
+
+  /// ULTRASOUND SETUP ///
+  pinMode(TRIGPIN, OUTPUT); // Sets the trigPin as an output
+  pinMode(ECHOPIN, INPUT);  // Sets the echoPin as an input
+  Serial.println("Car Count Ready!");
+
+  /// IR SENSOR SETUP ///
+  irrecv.enableIRIn(); // Start the receiver
+  Serial.println("Enabled IRin");
 }
 
 void loop() {
-  delay(100);
+  //IR_Detector();
+  //Car_Count();
+  delay(1000);
 }
 
 // callback for received data
@@ -26,25 +54,63 @@ void receiveData(int byteCount){
 
   while(Wire.available()) {
     number = Wire.read();
-    Serial.print(“data received: “);
+    Serial.print("data received: ");
     Serial.println(number);
     
-    if (number == 1)
-    {
-      if (state == 0){
-        print("led on"); // set the LED on
-        state = 1;
-      }
-      else
-      {
-        print("led off"); // set the LED off
-        state = 0;
-      }
-    }
   }
 }
 
 // callback for sending data
 void sendData(){
-  Wire.write(number);
+  if (number == 1)
+  {
+    Wire.write(carCount);
+  }
+  else if (number == 2)
+  {
+    carCount = 0;
+  }
+}
+
+// Car Count 
+void Car_Count()
+{
+  digitalWrite(TRIGPIN, LOW);
+  delayMicroseconds(2);
+ 
+  digitalWrite(TRIGPIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGPIN, LOW);
+ 
+  // Reads the echoPin, returns the sound wave travel time in
+  // microseconds
+  duration = pulseIn(ECHOPIN, HIGH);
+
+  // Calculating the distance
+  distance = duration*0.034/2;
+
+  //Prints the distance on the serial Monitor
+  Serial.print("Car Count Distance: ");
+  Serial.println(distance);
+
+  if (distance <= 150 && enableCount)
+  {
+    enableCount = false;
+    carCount += 1;
+    Serial.print("Car count: ");
+    Serial.println(carCount); 
+  }
+  else if (distance > 150)
+  {
+    enableCount = true; 
+  }
+}
+
+// Do we want an LED to light up instead?
+void IR_Detector()
+{
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    irrecv.resume();
+  }
 }
